c78fc6ace7f2c7b6b1ef0f59f4c6ff009bd8a163
James Nelson <jamestn529@gmail.com>
2021-11-10 21:02:44 -0700

    move demo SpriteList initialization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,13 @@ public class Main {
             System.out.println(tile);
         }
 
+        var sprites = new SpriteList(10);
+        sprites.add(0, new SpriteList.Sprite(0, 0, 5, 8, Jaylib.GREEN, null, new Raylib.Rectangle()));
+        sprites.add(0, new SpriteList.Sprite(0, 0, 11, 13, Jaylib.RED, null, new Raylib.Rectangle()));
         Raylib.InitWindow(800, 450, "Demo");
         while (!Raylib.WindowShouldClose()) {
             Raylib.ClearBackground(Jaylib.BLACK);
             Raylib.BeginDrawing();
-            var sprites = new SpriteList(10);
-            sprites.add(0, new SpriteList.Sprite(0, 0, 5, 8, Jaylib.GREEN, null));
-            sprites.add(0, new SpriteList.Sprite(0, 0, 11, 13, Jaylib.RED, null));
             sprites.drawAll();
             Raylib.EndDrawing();
         }
