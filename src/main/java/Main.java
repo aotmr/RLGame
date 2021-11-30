cfca44c58429fa39e49cf710c53e555b297d34b8
James Nelson <jamestn529@gmail.com>
2021-11-30 16:04:27 -0700

    add basic app loop using state manager

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
+import com.raylib.Raylib;
+import state.GameState;
+import state.StateMgr;
+
 public class Main {
+    public static void main(String[] args) {
+        var stateMgr = new StateMgr(new GameState());
+
+        Raylib.SetTargetFPS(60);
+        Raylib.InitWindow(800, 600, "window title");
+
+        while (!Raylib.WindowShouldClose()) {
+            var frameTime = Raylib.GetFrameTime();
+            stateMgr.update(frameTime);
+
+            Raylib.BeginDrawing();
+            stateMgr.draw();
+            Raylib.DrawFPS(0, 0);
+            Raylib.EndDrawing();
+        }
+
+        stateMgr.cleanup();
+    }
 }
