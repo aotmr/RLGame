6511bca86eca8a1351bb7adcd7f1666ea31c6a14
James Nelson <jamestn529@gmail.com>
2021-11-30 20:30:49 -0700

    add basic player controls, allowing for a simple walkabout (with a rectangle)

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,6 +1,7 @@
 package entity;
 
 import com.raylib.Jaylib;
+import com.raylib.Raylib;
 import draw.SpriteList;
 
 public class Player {
@@ -9,19 +10,48 @@ public class Player {
     }
 
     private static void update(Entity entity, double dt) {
+        final int WALKSPEED = 100;
+        final float FRICTION = 0.3f;
 
+        boolean keyLeft = Raylib.IsKeyDown(Raylib.KEY_LEFT);
+        boolean keyRight = Raylib.IsKeyDown(Raylib.KEY_RIGHT);
+        boolean keyUp = Raylib.IsKeyDown(Raylib.KEY_UP);
+        boolean keyDown = Raylib.IsKeyDown(Raylib.KEY_DOWN);
+
+        // The directional keys control a virtual joystick
+        float dirX = 0;
+        float dirY = 0;
+        if (keyLeft) dirX -= 1;
+        if (keyRight) dirX += 1;
+        if (keyUp) dirY -= 1;
+        if (keyDown) dirY += 1;
+
+        if (dirX != 0 || dirY != 0) {
+            float rnorm = 1 / (float)Math.sqrt(dirX * dirX + dirY * dirY);
+            dirX *= rnorm;
+            dirY *= rnorm;
+
+            // If the stick is tilted, accelerate the in that direction
+            entity.accelBy(dirX * WALKSPEED * FRICTION, dirY * WALKSPEED * FRICTION);
+            entity.clampVelocity(WALKSPEED);
+        } else {
+            // Otherwise, slow the player down
+            float vx = entity.getVelocityX();
+            float vy = entity.getVelocityY();
+            entity.accelBy(-FRICTION * vx, -FRICTION * vy);
+        }
     }
 
     private static void draw(Entity entity, SpriteList spriteList) {
         spriteList.add(new SpriteList.SpriteData(
                 entity.getX(),
                 entity.getY(),
-                20, 20,
+                16, 32,
                 0, 0,
                 0,
                 null,
                 null,
-                Jaylib.WHITE));
+                Jaylib.VIOLET));
     }
 
     public static Entity create(EntityList parent) {
